f7e1997844619a6d8a3350786bde1381eb5fd32c
Mahdi <mmahdikarimi2006@gmail.com>
2025-01-04 12:04:11 +0430

    button updated

--- components/shared/Button.tsx
+++ components/shared/Button.tsx
@@ -4,15 +4,21 @@ interface ButtonProps {
   children?: React.ReactNode;
   name: string;
   className?: string;
+  nameClass?: string;
 }
 
-export default function Button({ children, name, className }: ButtonProps) {
+export default function Button({
+  children,
+  name,
+  className,
+  nameClass,
+}: ButtonProps) {
   return (
     <button
       className={`bg-yellow-400 h-9 text-white px-4 rounded-lg flex items-center ${className}`}
     >
       <span>{children}</span>
-      <span className="p-6">{name}</span>
+      <span className={!nameClass ? "p-6" : nameClass}>{name}</span>
     </button>
   );
 }
