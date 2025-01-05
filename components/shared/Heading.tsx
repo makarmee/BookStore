87223fd0b0a7ae878ebb81e3366a85c5e08ec81d
Mahdi <mmahdikarimi2006@gmail.com>
2025-01-05 19:11:14 +0430

    heading updated

--- components/shared/Heading.tsx
+++ components/shared/Heading.tsx
@@ -6,6 +6,14 @@ interface HeadingProps {
 }
 export default function Heading({ content, className }: HeadingProps) {
   return (
-    <h1 className={!className ? "text-2xl p-2.5" : className}>{content}</h1>
+    <h1
+      className={
+        !className
+          ? "text-2xl max-sm:text-xl max-sm:my-2 m-0 max-sm:px-0 p-2.5"
+          : className
+      }
+    >
+      {content}
+    </h1>
   );
 }
