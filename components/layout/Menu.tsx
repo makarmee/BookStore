f5cfd241732123ef6262b65046bc35238253ab99
Mahdi <mmahdikarimi2006@gmail.com>
2025-01-06 23:41:49 +0430

    urls corrected

--- components/layout/Menu.tsx
+++ components/layout/Menu.tsx
@@ -7,20 +7,20 @@ export default function Menu() {
   return (
     <nav className="flex justify-between w-full h-9 gap-6 items-center">
       <div className="max-sm:me-6 md:me-16 min-h-5 min-w-5">
-        <img src="nav/Menu.svg" className="w-full h-full" />
+        <img src="/nav/Menu.svg" className="w-full h-full" />
       </div>
       <div className="bg-zinc-200 rounded-md w-72 flex flex-row py-2 h-9 me-auto">
-        <img src="nav/Search.svg" className="mx-4" />
+        <img src="/nav/Search.svg" className="mx-4" />
         <input
           placeholder="What are you looking for ?"
           className="bg-transparent w-full"
         />
       </div>
       <Link href="#" className="h-5 min-w-5 max-sm:hidden">
-        <img src="nav/Account.svg" className="w-full h-full" />
+        <img src="/nav/Account.svg" className="w-full h-full" />
       </Link>
       <Link href="#" className="h-5 min-w-5 max-sm:hidden">
-        <img src="nav/Favorite.svg" className="w-full h-full" />
+        <img src="/nav/Favorite.svg" className="w-full h-full" />
       </Link>
       <Button
         className="max-sm:circle square"
