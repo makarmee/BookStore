7e2714dfb801c014eb0375b39a18cbd183575ae4
Mahdi <mmahdikarimi2006@gmail.com>
2025-01-08 21:01:59 +0430

    footer; tablet-mode

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -5,11 +5,11 @@ import Button from "../shared/Button";
 
 export default function Footer() {
   return (
-    <footer className="flex flex-col justify-between gap-40 pt-8 w-full max-sm:p-4 max-sm:bg-[linear-gradient(120deg,#fc0,#FEAF38)]">
-      <div className="flex justify-between sm:px-8 max-sm:px-6 items-end sm:items-center max-sm:hidden flex-col md:flex-row sm:gap-24">
-        <div className="md:flex flex-col items-start gap-10 sm:hidden">
-          <img src="Logo.svg" className="h-24" />
-          <ul className="flex gap-10">
+    <footer className="flex flex-col justify-between gap-40 sm:gap-24 pt-8 w-full max-sm:p-4 max-sm:bg-[linear-gradient(120deg,#fc0,#FEAF38)]">
+      <div className="flex sm:justify-center justify-between sm:px-8 max-sm:px-6 md:items-end items-center max-sm:hidden flex-col-reverse md:flex-row md:gap-12 gap-24">
+        <div className="flex flex-col items-start md:gap-10 gap-20">
+          <img src="Logo.svg" className="h-24 mx-auto md:mx-0 sm:scale-75" />
+          <ul className="flex lg:gap-10 gap-6">
             <Link href="#">About</Link>
             <Link href="#">Features</Link>
             <Link href="#">Pricing</Link>
